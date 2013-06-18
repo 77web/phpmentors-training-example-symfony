206ea7b6315763c934a035d3954a9b9db3e45783
77web <info@77-web.com>
2013-06-18 20:49:06 +0900

    [UserRegistrationService] 登録時のアクティベーションキー生成機能

--- src/Example/UserRegistrationBundle/Domain/Service/UserRegistrationService.php
+++ src/Example/UserRegistrationBundle/Domain/Service/UserRegistrationService.php
@@ -5,6 +5,7 @@ namespace Example\UserRegistrationBundle\Domain\Service;
 use Doctrine\ORM\EntityManager;
 use Example\UserRegistrationBundle\Domain\Data\User;
 use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Core\Util\SecureRandomInterface;
 
 class UserRegistrationService
 {
@@ -18,14 +19,26 @@ class UserRegistrationService
      */
     private $passwordEncoder;
 
-    public function __construct(EntityManager $entityManager, PasswordEncoderInterface $passwordEncoder)
+    /**
+     * @var \Symfony\Component\Security\Core\Util\SecureRandomInterface
+     */
+    private $secureRandom;
+
+    /**
+     * @param EntityManager $entityManager
+     * @param PasswordEncoderInterface $passwordEncoder
+     * @param SecureRandomInterface $secureRandom
+     */
+    public function __construct(EntityManager $entityManager, PasswordEncoderInterface $passwordEncoder, SecureRandomInterface $secureRandom)
     {
         $this->entityManager = $entityManager;
         $this->passwordEncoder = $passwordEncoder;
+        $this->secureRandom = $secureRandom;
     }
 
     public function register(User $user)
     {
+        $user->setActivationKey(base64_encode($this->secureRandom->nextBytes(24)));
         $user->setPassword($this->passwordEncoder->encodePassword($user->getPassword(), User::SALT));
 
         $this->entityManager->getRepository('Example\UserRegistrationBundle\Domain\Data\User')->add($user);
--- src/Example/UserRegistrationBundle/Tests/Domain/Service/UserRegistrationServiceTest.php
+++ src/Example/UserRegistrationBundle/Tests/Domain/Service/UserRegistrationServiceTest.php
@@ -11,6 +11,7 @@ class UserRegistrationServiceTest extends TestCase
     {
         $em = \Phake::mock('Doctrine\ORM\EntityManager');
         $password = 'test';
+        $activationKey = 'activationkey';
 
         $userRepository = \Phake::mock('Example\UserRegistrationBundle\Domain\Data\UserRepository');
         \Phake::when($em)->getRepository('Example\UserRegistrationBundle\Domain\Data\User')->thenReturn($userRepository);
@@ -18,12 +19,17 @@ class UserRegistrationServiceTest extends TestCase
         $encoder = \Phake::mock('Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface');
         \Phake::when($encoder)->encodePassword($this->anything(), $this->anything())->thenReturn($password);
 
+        $secureRandom = \Phake::mock('Symfony\Component\Security\Core\Util\SecureRandomInterface');
+        \Phake::when($secureRandom)->nextBytes($this->anything())->thenReturn($activationKey);
+
         $user = \Phake::mock('Example\UserRegistrationBundle\Domain\Data\User');
         \Phake::when($user)->getPassword()->thenReturn($password);
 
-        $registrationService = new UserRegistrationService($em, $encoder);
+        $registrationService = new UserRegistrationService($em, $encoder, $secureRandom);
         $registrationService->register($user);
 
+        \Phake::verify($secureRandom)->nextBytes($this->isType(\PHPUnit_Framework_Constraint_IsType::TYPE_INT));
+        \Phake::verify($user)->setActivationKey($this->isType(\PHPUnit_Framework_Constraint_IsType::TYPE_STRING));
         \Phake::verify($user)->getPassword();
         \Phake::verify($encoder)->encodePassword($this->isType(\PHPUnit_Framework_Constraint_IsType::TYPE_STRING), $this->isType(\PHPUnit_Framework_Constraint_IsType::TYPE_STRING));
         \Phake::verify($user)->setPassword($this->isType(\PHPUnit_Framework_Constraint_IsType::TYPE_STRING));
