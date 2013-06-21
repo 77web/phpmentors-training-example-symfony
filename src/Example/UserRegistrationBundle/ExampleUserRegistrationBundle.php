13ee7defa806f7d1c4a374a078c2dee36aa030ca
77web <info@77-web.com>
2013-06-22 00:00:56 +0900

    [ExampleUserRegistrationBundle] SwiftMailerで日本語メール送信ができるようにバンドルのブート時の設定を追加した

--- src/Example/UserRegistrationBundle/ExampleUserRegistrationBundle.php
+++ src/Example/UserRegistrationBundle/ExampleUserRegistrationBundle.php
@@ -6,4 +6,19 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ExampleUserRegistrationBundle extends Bundle
 {
+    public function boot()
+    {
+        $this->configureMail();
+    }
+
+    protected function configureMail()
+    {
+        \Swift::init(function(){
+                \Swift_DependencyContainer::getInstance()
+                    ->register('mime.qpheaderencoder')
+                    ->asAliasOf('mime.base64headerencoder')
+                ;
+                \Swift_Preference::getInstance()->setCharset('iso-2022-jp');
+            });
+    }
 }
